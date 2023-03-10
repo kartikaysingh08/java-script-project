da280dc2647b3a310c092b4f5e3ea647ebe98e4d
kartikay singh <121293713+kartikaysingh08@users.noreply.github.com>
2023-03-11 02:18:08 +0530

    Add files via upload

---
+++ passenger counter app/index.js
@@ -0,0 +1,147 @@
+//document.getElementbyId("count-el").innerText = 5//
+/*let a= 45;
+let age=a;
+console.log(age);
+let myage=21
+let humandogratio=3
+let mydogage = myage * humandogratio
+console.log(mydogage)
+let bonuspoints=50
+bonuspoints=bonuspoints+50
+console.log(bonuspoints)
+bonuspoints=bonuspoints-75
+console.log(bonuspoints)
+bonuspoints=bonuspoints+45
+console.log(bonuspoints)
+function increment(){
+    console.log("The button is clicked")
+}
+function countdown(){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+}
+countdown()
+function number(){
+    console.log(42)
+}
+number()
+let lap1=31
+let lap2=32
+let lap3=33
+function sum(){
+    let d = lap1 + lap2 + lap3
+    console.log(d)
+}
+sum()
+let lap1=31
+let lap2=32
+let lap3=33
+function sum(){
+    //let d = lap1 + lap2 + lap3
+    console.log(lap1 + lap2 + lap3)
+}
+sum()
+let lapscompleted=0
+function zero(){
+    let d=(lapscompleted+1)
+    console.log(d)
+    console.log(d+1)
+    console.log(lapscompleted+1)
+}
+zero()
+zero()
+zero()
+let lapsCompleted=0
+function incrementLap(){
+     lapsCompleted = lapsCompleted + 1
+     console.log(lapsCompleted)
+}
+incrementLap()
+incrementLap()
+incrementLap()
+
+//console.log(lapsCompleted)
+let countEl = document.getElementById("count-el")
+//console.log(countEl)
+let count=0
+function increment(){
+    count =count+1
+countEl.innerText=count
+  //  console.log(count)
+}
+increment()
+let countEl= document.getElementById("count-el")
+let count=0
+function increment(){
+count =count+1
+countEl.innerText=count
+}
+function save(){
+    console.log(count)
+}
+let name ="kartik"
+let greeting= "hi,ny name"
+let str2=(greeting +""+ name)
+console.log(str2)
+let welcome = document.getElementById("welcome-el")
+let names ='kartik'
+let greeting ='welcome to our page'
+welcome.innerText=(names + greeting)
+let countEl =document.getElementById("count-el")
+let saveEl =document.getElementById("save-el")
+let count =0
+console.log(saveEl)
+function increment(){
+    count +=1 
+    countEl.innerText =count  
+    console.log(count) 
+}
+increment()
+//console.log(count)
+function save(){
+let countstr = count+ " - "
+saveEl.textContent +=countstr
+countEl.innerText = 0
+count=0
+console.log(count)
+}
+save()
+let name = "linda"
+let greeting ="hi there"
+function zoom(){
+    let h = name +greeting
+    console.log(h) 
+}
+zoom()
+
+let mypoints =3
+ function add3points(){
+let count =mypoints+1
+console.log(count)
+ }
+add3points()
+ function removepoints(){
+
+ }*
+let zy =document.getElementById("error")
+function purchase(){
+    zy.innerText()
+}*/
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.innerText = 0
+    count=0
+}
+
